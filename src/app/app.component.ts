1a2ac25a402b235d7cb42b8c257f18a7c1197965
Hüveyda Tükle <huveydatukle@Huveydas-MacBook-Air-2.local>
2024-01-29 16:27:52 +0000

    created auth-modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserModule } from './user/user.module';
+import { NavComponent } from './nav/nav.component';
+import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    UserModule,
+    NavComponent,
+    AuthModalComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

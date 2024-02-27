8b2f0cdb71d43d8a5999100a73a9a4a7a595ba92
Hüveyda Tükle <huveydatukle@Huveydas-MacBook-Air-2.local>
2024-02-27 17:51:32 +0000

    created shared module

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-auth-modal',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './auth-modal.component.html',
-  styleUrl: './auth-modal.component.css'
+  styleUrl: './auth-modal.component.css',
 })
-export class AuthModalComponent {
-
-}
+export class AuthModalComponent {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AuthModalComponent],
+  imports: [CommonModule, AuthModalComponent, SharedModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
